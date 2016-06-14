f283e37ac5736d8fbe75219d70a041a9d5d4ee76
Alejandro Zürcher <alejandro.zurcher@gmail.com>
2016-06-14 00:00:33 -0300

    Adding support for custom font

--- app/src/main/java/az/simplecalc/Calculator.java
+++ app/src/main/java/az/simplecalc/Calculator.java
@@ -3,11 +3,23 @@ package az.simplecalc;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
+import az.simplecalc.util.FontsOverride;
+
 public class Calculator extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+
+        setupCustomFont();
+
         setContentView(R.layout.activity_calculator);
+    }
+
+    private void setupCustomFont() {
+        FontsOverride.setDefaultFont(this, "DEFAULT", "Montserrat-Light.ttf");
+        FontsOverride.setDefaultFont(this, "MONOSPACE", "Montserrat-Light.ttf");
+        FontsOverride.setDefaultFont(this, "SERIF", "Montserrat-Light.ttf");
+        FontsOverride.setDefaultFont(this, "SANS_SERIF", "Montserrat-Light.ttf");
     }
 }
---
+++ app/src/main/java/az/simplecalc/util/FontsOverride.java
@@ -0,0 +1,34 @@
+package az.simplecalc.util;
+
+import android.content.Context;
+import android.graphics.Typeface;
+
+import java.lang.reflect.Field;
+
+/**
+ * Created by az on 13/06/16.
+ */
+public class FontsOverride {
+
+    public static void setDefaultFont(Context context,
+                                      String staticTypefaceFieldName, String fontAssetName) {
+        final Typeface regular = Typeface.createFromAsset(context.getAssets(),
+                fontAssetName);
+        replaceFont(staticTypefaceFieldName, regular);
+    }
+
+    protected static void replaceFont(String staticTypefaceFieldName,
+                                      final Typeface newTypeface) {
+        try {
+            final Field staticField = Typeface.class
+                    .getDeclaredField(staticTypefaceFieldName);
+            staticField.setAccessible(true);
+            staticField.set(null, newTypeface);
+        } catch (NoSuchFieldException e) {
+            e.printStackTrace();
+        } catch (IllegalAccessException e) {
+            e.printStackTrace();
+        }
+    }
+
+}
