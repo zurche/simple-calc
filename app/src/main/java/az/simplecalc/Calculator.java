68a837c7b5dd541718d2d8e612fd627c70d286ea
Alejandro Zürcher <alejandro.zurcher@gmail.com>
2016-06-15 22:59:12 -0300

    Adding EvalEx to project

--- app/src/main/java/az/simplecalc/Calculator.java
+++ app/src/main/java/az/simplecalc/Calculator.java
@@ -3,6 +3,10 @@ package az.simplecalc;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
+import com.udojava.evalex.Expression;
+
+import java.math.BigDecimal;
+
 import az.simplecalc.util.FontsOverride;
 
 public class Calculator extends AppCompatActivity {
@@ -12,6 +16,14 @@ public class Calculator extends AppCompatActivity {
         super.onCreate(savedInstanceState);
 
         setupCustomFont();
+
+        BigDecimal result = null;
+
+        Expression expression = new Expression("20*10+15");
+
+        result = expression.eval();
+
+        System.out.println("20*10+15 = " + result);
 
         setContentView(R.layout.activity_calculator);
     }
